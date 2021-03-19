3c90a73eef4f1e4c6cd11bc695f0484b40e12f5c
azizamraoui <amraouiaziz4@gmail.com>
2021-03-19 20:14:58 +0100

    jsx

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import imagesrc from "./images/imageInSrc.jpg"
+
+function App() {
+  return (
+    <div className="App">
+ <>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">Amraoui aziz</h1>
+   
+    <img src={imagesrc} alt="town2"/>
+   
+    <img src="/imageInPublic.jpg" alt="town1" />
+  </div>
+  <video width={320} height={240} controls autoPlay>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</>
+    </div>
+  );
+}
+
+export default App;
